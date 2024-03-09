693f036079a2bb7884f00396657f5083f19fe445
jaickler <25970923+jaickler@users.noreply.github.com>
2024-03-09 11:13:50 -0500

    Test: Add GetPlanetsTest

--- HellDiversDotNetTests/SuperEarthConnectionTests.cs
+++ HellDiversDotNetTests/SuperEarthConnectionTests.cs
@@ -44,5 +44,15 @@ namespace HellDiversDotNetTests
             latestEvent.Should().BeOfType<Event>(
                 because: "the function should return an event.");
         }
+
+        [TestMethod]
+        public async Task GetPlanetsReturnsList()
+        {
+            var planets = await superEarthConnection.GetPlanetsAsync();
+
+            planets.Should().NotBeNull();
+            planets.Should().BeOfType<List<Planet>>(
+                because: "the function should return a list of planets.");
+        }
     }
 }
