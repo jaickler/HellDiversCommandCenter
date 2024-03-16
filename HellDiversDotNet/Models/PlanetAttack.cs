907761c7e352f4efd1efe72525a2d6ff0fbb54e6
jaickler <25970923+jaickler@users.noreply.github.com>
2024-03-15 21:17:30 -0400

    Model: Add Planet Attack Model

---
+++ HellDiversDotNet/Models/PlanetAttack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HellDiversDotNet.Models
+{
+    public class PlanetAttack
+    {
+        [JsonPropertyName("source")]
+        public Planet? Source { get; set; }
+
+        [JsonPropertyName("target")]
+        public Planet? Target { get; set; }
+
+        public PlanetAttack FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<PlanetAttack>(json);
+        }
+    }
+}
