6a6738d73a35bb290987899a2add4fb3fc353df9
jaickler <25970923+jaickler@users.noreply.github.com>
2024-03-15 21:17:16 -0400

    Model: Add Joint Operation Model

---
+++ HellDiversDotNet/Models/JointOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HellDiversDotNet.Models
+{
+    public class JointOperation
+    {
+        [JsonPropertyName("hq_node_index")]
+        public uint? HqNodeIndex { get; set; }
+
+        [JsonPropertyName("id")]
+        public uint? Id { get; set; }
+
+        [JsonPropertyName("planet")]
+        public Planet? Planet { get; set; }
+
+        public JointOperation FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<JointOperation>(json);
+        }
+    }
+}
